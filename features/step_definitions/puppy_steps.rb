d059a95120a2b98aa62b58e75c96608d1f7e3263
Fernando Cuenca <fernando_cuenca@yahoo.com.ar>
2017-02-19 18:27:47 -0500

    refactored checkout steps into page object

--- features/step_definitions/puppy_steps.rb
+++ features/step_definitions/puppy_steps.rb
@@ -16,25 +16,23 @@ When(/^I click the Complete the Adoption button$/) do
 end
 
 When(/^I enter "([^"]*)" in the name field$/) do |name|
-  @browser.find_element(:id, "order_name").send_keys(name)
+  on(CheckoutPage).name = name
 end
 
 When(/^I enter "([^"]*)" in the address field$/) do |address|
-  @browser.find_element(:id, "order_address").send_keys(address)
+  on(CheckoutPage).address = address
 end
 
 When(/^I enter "([^"]*)" in the email field$/) do |email|
-  @browser.find_element(:id, "order_email").send_keys(email)
+  on(CheckoutPage).email = email
 end
 
 When(/^I select "([^"]*)" from the pay with dropdown$/) do |pay_type|
-  dropDownMenu = @browser.find_element(:id, "order_pay_type")
-  option = Selenium::WebDriver::Support::Select.new(dropDownMenu)
-  option.select_by(:text, pay_type)
+  on(CheckoutPage).pay_type = pay_type
 end
 
 When(/^I click the Place Order button$/) do
-  @browser.find_element(:xpath, '//input[@value="Place Order"]').submit
+  on(CheckoutPage).place_order
 end
 
 Then(/^I should see "([^"]*)"$/) do |expected|
